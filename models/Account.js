4d122bc89d6368bce210602adfe133052352df29
thanh phan <thanhpvse62274@fpt.edu.vn>
2020-05-31 18:16:46 +0700

    edit model

--- models/Account.js
+++ models/Account.js
@@ -31,24 +31,7 @@ const Account = db.define(
   }
  
 );
-//role
-Account.belongsTo(Role,{
-  foreignKey: "role_id",
-  sourceKey: "id"
-});
-Role.hasMany(Account,{
-  foreignKey: "role_id",
-  sourceKey: "id"
-});
-//department
-Account.belongsTo(Department,{
-  foreignKey:"department_id",
-  sourceKey: "id"
-});
-Department.hasMany(Account,{
-  foreignKey: "department_id",
-  sourceKey: "id"
-});
+
 //status
 Account.belongsTo(AccountOperatorStatus,{
   foreignKey: "status_id",
