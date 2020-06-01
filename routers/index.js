64bcb336bfdd346e5f71b6abeb3dafe580653b4c
sonnguyen97 <nguyencongson163@gmail.com>
2020-06-01 22:24:17 +0700

    update folder is required lower case

--- routers/index.js
+++ routers/index.js
@@ -6,7 +6,7 @@ router.use("/departments", require("./api/department/department.route"));
 router.use("/roles", require("./api/role/role.route"));
 router.use("/teams", require("./api/team/team.route"));
 router.use("/auth", require("./api/authentication/auth.route"));
-router.use("/accountOpetatorStatus", require("./api/AccountOperatorStatus/AccountOperatorStatus.route"));
+router.use("/accountOpetatorStatus", require("./api/accountOperatorStatus/AccountOperatorStatus.route"));
 // router.use("/accountTeam", require("./api/account_team/account_team.route"));
 router.use("/teamEmployee", require("./api/team_employee/team_employee.route"));
 router.use("/employees", require("./api/employee/employee.route"));
