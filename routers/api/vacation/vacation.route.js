593bd638d7ce8db3058c9cff04eb55e562338d8a
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2020-08-11 06:52:40 +0700

    update fix

--- routers/api/vacation/vacation.route.js
+++ routers/api/vacation/vacation.route.js
@@ -25,9 +25,11 @@ router.post("/create", async(req,res)=> {
 router.put("/update", async(req,res)=> {
     var request = req.body;
     const vacation =new Vacation_Employee();
-    vacation = request;
     try {
         if(vacation != null && vacation != undefined){
+            vacation.start_date = request.start_date;
+            vacation.end_date = request.end_date;
+            vacation.employee_id = request.employee_id;
             await vacation_dao.updateVacation(vacation);
             res.status(200).json("update success");
         }else{
