d8a82ba641210f5954413697f19a7881d6074055
thongnd1 <thongnd21@gmail.com>
2020-08-21 14:25:18 +0700

    update show emp in department

--- routers/api/department/department.route.js
+++ routers/api/department/department.route.js
@@ -24,7 +24,7 @@ router.get("/:id", async (req, res) => {
     }
 });
 
-router.get("empdep/:id", async (req, res) => {
+router.get("/empdep/:id", async (req, res) => {
     const id = req.params.id;
     try {
         const empList = await department_dao.getAllEmpByDep(id);
